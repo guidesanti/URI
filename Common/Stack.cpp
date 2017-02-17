7f1b0ebb29f2e20f8fdf14f23ff0c090e65e2350
Guilherme A. de Santi <g.santi@samsung.com>
2017-02-17 10:58:48 -0200

    Project rename.
    Renamed project Collections to Common.

---
+++ Common/Stack.cpp
@@ -0,0 +1,134 @@
+#include "Stack.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+struct StackFrame {
+	int			data;
+	StackFrame*	next;
+};
+
+struct Stack {
+	StackFrame*	head;
+	int			size;
+};
+
+Stack* Stack_Create() {
+	Stack* stack = (Stack*)malloc(sizeof(Stack));
+
+	stack->head = NULL;
+	stack->size = 0;
+
+	return stack;
+}
+
+int Stack_Destroy(Stack* stack) {
+	if (!stack) {
+		return STACK_NULL_ARG;
+	}
+
+	StackFrame* currentStackFrame = stack->head;
+	StackFrame* nextStackFrame = NULL;
+	while (currentStackFrame) {
+		nextStackFrame = currentStackFrame->next;
+		free(currentStackFrame);
+		currentStackFrame = nextStackFrame;
+	}
+
+	free(stack);
+
+	return STACK_OK;
+}
+
+int Stack_Clear(Stack* stack) {
+	if (!stack) {
+		return STACK_NULL_ARG;
+	}
+
+	StackFrame* currentStackFrame = stack->head;
+	StackFrame* nextStackFrame = NULL;
+	while (currentStackFrame) {
+		nextStackFrame = currentStackFrame->next;
+		currentStackFrame->next = NULL;
+		free(currentStackFrame);
+		currentStackFrame = nextStackFrame;
+	}
+
+	stack->head = NULL;
+	stack->size = 0;
+
+	return STACK_OK;
+}
+
+int Stack_Push(Stack* stack, int data) {
+	if (!stack) {
+		return STACK_NULL_ARG;
+	}
+
+	StackFrame* stackFrame = (StackFrame*)malloc(sizeof(StackFrame));
+	if (!stackFrame) {
+		return STACK_OUT_OF_MEMORY;
+	}
+
+	stackFrame->data = data;
+	stackFrame->next = stack->head;
+	stack->head = stackFrame;
+	stack->size++;
+
+	return STACK_OK;
+}
+
+int Stack_Pop(Stack* stack, int* data) {
+	if ((!stack) || (!data)) {
+		return STACK_NULL_ARG;
+	}
+
+	if (!stack->head) {
+		*data = NULL;
+		return STACK_EMPTY;
+	}
+
+	StackFrame* stackFrame = stack->head;
+	*data = stackFrame->data;
+	stack->head = stackFrame->next;
+	stack->size--;
+	free(stackFrame);
+
+	return STACK_OK;
+}
+
+int Stack_Size(Stack* stack, int* size) {
+	if ((!stack) || (!size)) {
+		return STACK_NULL_ARG;
+	}
+
+	*size = stack->size;
+
+	return STACK_OK;
+}
+
+int Stack_ToString(Stack* stack, char** str) {
+	if (!str) {
+		return STACK_NULL_ARG;
+	}
+
+	*str = (char*)malloc(1024 * sizeof(char));
+
+	if (!stack) {
+		sprintf(*str, "null");
+		return STACK_OK;
+	}
+
+	StackFrame* stackFrame = stack->head;
+	int index = sprintf(*str, "{ ");
+	while (stackFrame) {
+		if (stackFrame->next) {
+			index += sprintf(*str + index, "%d -> ", stackFrame->data);
+		} else {
+			index += sprintf(*str + index, "%d", stackFrame->data);
+		}
+		stackFrame = stackFrame->next;
+	}
+	sprintf(*str + index, " }");
+
+	return STACK_OK;
+}
---
+++ Common/io.cpp
@@ -0,0 +1,72 @@
+#include "io.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+#define DEFAULT_LINE_LENGTH	(100)
+#define DEFAULT_WORD_LENGTH	(15)
+
+char* readLine(void) {
+	int lineLength = DEFAULT_LINE_LENGTH;
+	char* line = (char*)malloc(lineLength * sizeof(char));
+	int index = 0;
+	char ch = 0;
+
+	if (!line) {
+		return NULL;
+	}
+
+	while (1) {
+		ch = fgetc(stdin);
+		if ((ch == '\n') || (ch == EOF)) {
+			break;
+		}
+
+		if (index == lineLength) {
+			char* newLine = (char*)realloc(line, 2 * lineLength);
+			if (!newLine) {
+				free(line);
+				return NULL;
+			}
+			line = newLine;
+			lineLength *= 2;
+		}
+
+		line[index++] = ch;
+	}
+	line[index] = '\0';
+	
+	return line;
+}
+
+char* readWord(void) {
+	int wordLength = DEFAULT_WORD_LENGTH;
+	char* word = (char*)malloc(wordLength * sizeof(char));
+	int index = 0;
+	char ch = 0;
+
+	if (!word) {
+		return NULL;
+	}
+
+	while (1) {
+		ch = fgetc(stdin);
+		if ((ch == ' ') || (ch == '\n') || (ch == EOF)) {
+			break;
+		}
+
+		if (index == wordLength) {
+			char* newLine = (char*)realloc(word, 2 * wordLength);
+			if (!newLine) {
+				free(word);
+				return NULL;
+			}
+			word = newLine;
+			wordLength *= 2;
+		}
+
+		word[index++] = ch;
+	}
+	word[index] = '\0';
+
+	return word;
+}
