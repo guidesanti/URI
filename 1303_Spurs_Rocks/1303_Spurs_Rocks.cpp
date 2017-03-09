3ad479b1c5f0b172f2bcf91fa348dc0f61877da1
Guilherme A. de Santi <g.santi@samsung.com>
2017-03-09 14:35:03 -0300

    1911 Help Girafales
    Solved the problem 1911 Help Girafales.
    Partial solution of problem 1303 Spours Rocks.

---
+++ 1303_Spurs_Rocks/1303_Spurs_Rocks.cpp
@@ -0,0 +1,142 @@
+#include <stdio.h>
+
+// Constraints
+#define MAX_NUMBER_OF_TEAMS	(100)	// MAximum number of teams
+
+// Data structures
+typedef struct Team Team;
+
+struct Team {
+	int		number;
+	int		pointsScored;
+	int		pointsReceived;
+	int		points;
+	double	averageBasket;
+	Team*	next;
+};
+
+// Data
+int numberOfTeams = 0;				// The number of teams
+int numberOfMatches = 0;			// The number of matches
+int instanceNumber = 0;				// The instance number
+Team teams[MAX_NUMBER_OF_TEAMS];	// The teams of the league
+Team* rank = NULL;					// The rank of the teams
+int i = 0;							// Loop control
+
+// Functions
+int Compare(Team* t1, Team* t2) {
+	if (t1->points > t2->points) {
+		return 1;
+	} else if (t1->points < t2->points) {
+		return 2;
+	} else {
+		if (t1->averageBasket > t2->averageBasket) {
+			return 1;
+		} else if (t1->averageBasket < t2->averageBasket) {
+			return 2;
+		} else {
+			if (t1->pointsScored > t2->pointsScored) {
+				return 1;
+			} else if (t1->pointsScored < t2->pointsScored) {
+				return 2;
+			} else{
+				if (t1->number < t2->number) {
+					return 1;
+				}
+				else {
+					return 2;
+				}
+			}
+		}
+	}
+}
+
+int main() {
+	while (1) {
+		// Read the number of teams
+		scanf("%d", &numberOfTeams);
+
+		// Check stop condition
+		if (!numberOfTeams) {
+			break;
+		}
+
+		// Reset
+		for (i = 0; i < numberOfTeams; i++) {
+			teams[i].number = (i + 1);
+			teams[i].pointsScored = 0;
+			teams[i].pointsReceived = 0;
+			teams[i].points = 0;
+			teams[i].averageBasket = 0.0;
+			teams[i].next = NULL;
+		}
+
+		// Read the match results
+		numberOfMatches = ((numberOfTeams * (numberOfTeams - 1)) / 2);
+		for (i = 0; i < numberOfMatches; i++) {
+			int x;
+			int y;
+			int z;
+			int w;
+			scanf("%d %d %d %d", &x, &y, &z, &w);
+			x--;
+			z--;
+			teams[x].pointsScored += y;
+			teams[x].pointsReceived += w;
+			teams[z].pointsScored += w;
+			teams[z].pointsReceived += y;
+			if (y > w) {
+				teams[x].points += 2;
+				teams[z].points += 1;
+			} else if (x < y) {
+				teams[x].points += 1;
+				teams[z].points += 2;
+			} else {
+				// It shall not reach here
+			}
+		}
+
+		// Calculate the average basket for each team
+		for (i = 0; i < numberOfTeams; i++) {
+			teams[i].averageBasket = (double)(teams[i].pointsScored);
+			if (teams[i].pointsReceived > 0) {
+				teams[i].averageBasket /= (double)(teams[i].pointsReceived);
+			}
+		}
+
+		// Rank the teams
+		rank = &teams[0];
+		for (i = 1; i < numberOfTeams; i++) {
+			if (Compare(&teams[i], rank) == 1) {
+				teams[i].next = rank;
+				rank = &teams[i];
+			} else {
+				Team* team = rank;
+				while (team->next) {
+					if (Compare(&teams[i], team->next) == 1) {
+						teams[i].next = team->next;
+						team->next = &teams[i];
+						break;
+					}
+					team = team->next;
+				}
+				if (!team->next) {
+					team->next = &teams[i];
+				}
+			}
+		}
+
+		// Print the result
+		if (instanceNumber) {
+			printf("\n");
+		}
+		printf("Instancia %d\n", ++instanceNumber);
+		while (rank) {
+			printf("%d ", rank->number);
+			rank = rank->next;
+		}
+		printf("\n");
+	}
+
+	return 0;
+}
---
+++ 1911_Help_Girafales/1911_Help_Girafales.cpp
@@ -0,0 +1,84 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+// Constraints
+#define MIN_NUMBER_OF_STUDENTS	1
+#define MAX_NUMBER_OF_STUDENTS	50
+#define MIN_NUMBER_OF_ATENDEES	0
+#define MAX_NUMBER_OF_ATENDEES	50
+#define MAX_NAME_LENGTH			20
+#define MAX_SIGNATURE_LENGTH	20
+
+// Data structures
+typedef struct Student Student;
+
+struct Student {
+	char name[MAX_NAME_LENGTH + 1];
+	char signature[MAX_SIGNATURE_LENGTH + 1];
+};
+
+// Data
+int numberOfStudents = 0;					// The number of students in the class
+int numberOfAttendees = 0;					// The number of students attending the class
+Student students[MAX_NUMBER_OF_STUDENTS];	// The students list
+int result = 0;								// The number of false signatures
+int i = 0;									// Loop control
+int j = 0;									// Loop control
+int k = 0;									// Loop control
+int count = 0;								// Count the number of differences between two signatures
+
+// Functions
+int main() {
+	while (1) {
+		// Read the number of students
+		scanf("%d", &numberOfStudents);
+
+		// Check the stop condition
+		if (!numberOfStudents) {
+			break;
+		}
+
+		// Reset
+		result = 0;
+		memset(students, 0, sizeof(students));
+
+		// Read the students names and signatures
+		for (i = 0; i < numberOfStudents; i++) {
+			scanf("%s %s", students[i].name, students[i].signature);
+		}
+
+		// Read the number of attendees
+		scanf("%d", &numberOfAttendees);
+
+		// Read the studants names and signatures of the day
+		char name[MAX_NAME_LENGTH + 1];
+		char signature[MAX_SIGNATURE_LENGTH + 1];
+		memset(signature, 0, sizeof(signature));
+		for (i = 0; i < numberOfAttendees; i++) {
+			scanf("%s %s", name, signature);
+
+			// Check the name and signature
+			for (j = 0; j < numberOfStudents; j++) {
+				if (strcmp(students[j].name, name) == 0) {
+					count = 0;
+					for (k = 0; k < MAX_SIGNATURE_LENGTH; k++) {
+						if (students[j].signature[k] != signature[k]) {
+							count++;
+							if (count > 1) {
+								result++;
+								break;
+							}
+						}
+					}
+					break;
+				}
+			}
+		}
+
+		// Print the result
+		printf("%d\n", result);
+	}
+
+	return 0;
+}
